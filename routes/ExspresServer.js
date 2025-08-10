ecfdcc766af53dec104c59142cbd9bd4fc6adace
Moti7950 <shalom7950@gmail.com>
2025-08-10 12:56:19 +0300

    add css file

--- routes/ExspresServer.js
+++ routes/ExspresServer.js
@@ -3,8 +3,12 @@ import adminRouter from "./adminRouter.js"
 import complaintRouter from "./complaintRouter.js"
 
 const complaintsServer = express()
+
+complaintsServer.use(express.json());
+
 complaintsServer.use(express.urlencoded({ extended: true }));
 
+complaintsServer.use(express.static("public"));
 
 complaintsServer.use(express.json())
 
